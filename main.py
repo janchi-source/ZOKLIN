711388e9007dd41288f877293e8268d984148e16
janchi-source <124387739+janchi-source@users.noreply.github.com>
2025-03-03 10:02:42 +0100

    artricels plus photos

--- main.py
+++ main.py
@@ -13,14 +13,33 @@ app = flask.Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+@app.route('/houses')
+def houses():
+    return render_template('houses.html')
+
+@app.route('/garden')
+def garden():
+    return render_template('garden.html')
+
+@app.route('/offices')
+def offices():
+    return render_template('offices.html')
+
+@app.route('/cars')
+def cars():
+    return render_template('cars.html')
+
+@app.route('/mental')
+def mental():
+    return render_template('mental.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
